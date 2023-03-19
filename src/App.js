6baf7939668db219336703b4e84290f3a9451987
Corey Sawin <sawincp@gmail.com>
2023-03-19 17:16:40 -0400

    created app components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import NavBar from './NavBar';
+import HeroList from './HeroList';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
