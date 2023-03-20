3e19a664f44aeda709f4d3ae5d37b7875a51b58f
Corey Sawin <sawincp@gmail.com>
2023-03-20 12:32:16 -0400

    update component names, use Routes instead of Switch, add HeroForm component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './Home'
+import Heros from './Heros';
+import HeroForm from './HeroForm';
 import NavBar from './NavBar';
-import HeroList from './HeroList';
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  return (
+  <div className="App">
+    <Routes>
+      <Route  exact path ="/" Component={Home}/>
+      <Route  exact path ="/heros" Component={Heros}/>
+      <Route exact path='/heros/new' Component={HeroForm}/>
+      <Route  path ="/heros/:id" Component={Heros}/>
+  
+
+    </Routes>
+
+
+  </div>
+   
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  </BrowserRouter>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
