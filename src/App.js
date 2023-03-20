21e5b9cd45889b876062e14adff5de065bf3d1e0
Corey Sawin <sawincp@gmail.com>
2023-03-20 13:10:48 -0400

    update NavBar component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
 
   return (
   <div className="App">
+    <NavBar />
     <Routes>
       <Route  exact path ="/" Component={Home}/>
       <Route  exact path ="/heros" Component={Heros}/>
