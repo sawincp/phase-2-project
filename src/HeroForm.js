942ab9c5efc7b5304600909708a69a90cbfae566
Corey Sawin <sawincp@gmail.com>
2023-03-27 16:49:11 -0400

    Updated db.json and updated JSX in HeroForm

--- src/HeroForm.js
+++ src/HeroForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"
+import { Form, Button, Col, Row } from 'react-bootstrap';
 
 const HeroForm = ()=>{
 
@@ -55,32 +56,98 @@ const HeroForm = ()=>{
         <div className="HeroForm">
             <h1>Create your own Hero!</h1>
             <hr></hr>
-            <form className="Form" onSubmit={handleSubmit}>
-                <label> Name </label>
-                <input name="name" onChange={handleInputChange} type="text"/><br/>
-                <label> Alias </label>
-                <input name="alias" onChange={handleInputChange} type="text"/><br/>
-                <label> Place of Birth </label>
-                <input name="place of birth" onChange={handleInputChange} type="text"/><br/>
-                <label> First Appeared </label>
-                <input name="first appreance" onChange={handleInputChange} type="text"/><br/>
-                <label> Intelligence </label>
-                <input name="intelligence" onChange={handleInputChange} type="text"/><br/>
-                <label> Strength </label>
-                <input name="strength" onChange={handleInputChange} type="text"/><br/>
-                <label> Speed </label>
-                <input name="speed" onChange={handleInputChange} type="text"/><br/>
-                <label> Durability </label>
-                <input name="durability" onChange={handleInputChange} type="text"/><br/>
-                <label> Power </label>
-                <input name="power" onChange={handleInputChange} type="text"/><br/>
-                <label> Combat </label>
-                <input name="combat" onChange={handleInputChange} type="text"/><br/>
-                <label> Image </label>
-                <input name="image" onChange={handleInputChange} type="text"/><br/>
-                <input type="submit"/>
+            <Form onSubmit={handleSubmit}>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="name">
+                        <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="name"
+                                onChange={handleInputChange}
+                                />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="alias">
+                        <Form.Label>Alias</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="name"
+                                onChange={handleInputChange}
+                                />
+                    </Form.Group>
+                </Row>
+                
+                <Row>
+                    <Form.Group as={Col} controlId="intelligence">
+                        <Form.Label>Intelligence</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="intelligence"
+                                onChange={handleInputChange}
+                                />
+                    </Form.Group>
+                
+                    <Form.Group as={Col} controlId="strength">
+                        <Form.Label>Strength</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="strength"
+                                onChange={handleInputChange}
+                                />
+                    </Form.Group>
+                </Row>
 
-            </form>
+                <Row>
+                    <Form.Group as={Col} controlId="speed">
+                        <Form.Label>Speed</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="speed"
+                                onChange={handleInputChange}
+                                />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="durability">
+                        <Form.Label>Durability</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="durability"
+                                onChange={handleInputChange}
+                                />
+                    </Form.Group>
+                </Row>
+                
+                <Row>
+                    <Form.Group as={Col} controlId="power">
+                        <Form.Label>Power</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="power"
+                                onChange={handleInputChange}
+                                />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="combat">
+                        <Form.Label>Combat</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="combat"
+                                onChange={handleInputChange}
+                                />
+                    </Form.Group>    
+                </Row>
+                
+                <Form.Group as={Col} controlId="image">
+                        <Form.Label>Image</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="image"
+                                onChange={handleInputChange}
+                                />
+                </Form.Group>    
+                
+                <br></br>
+                
+                <Button type="submit">Submit</Button>
+            </Form>
+            
         </div>
     )
 }
